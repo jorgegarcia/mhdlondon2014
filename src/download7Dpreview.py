dbd4da94a328e2ed85821a05cc93494dfb3d6c5e
Jorge Garcia <info@jorgegarciamartin.com>
2014-12-13 15:37:48 +0000

    fftm main script and preview downloads util function updated

---
+++ src/download7Dpreview.py
@@ -0,0 +1,34 @@
+from py7D import api_settings
+import oauth2 as oauth
+import time
+import pycurl
+from io import BytesIO
+
+
+def write7DPreview( filename, previewID ):
+    consumer_key = api_settings.oauthkey
+    consumer_secret = api_settings.secret
+
+    consumer = oauth.Consumer(consumer_key, consumer_secret)
+    request_url = "http://previews.7digital.com/clip/" + str(previewID)
+
+    req = oauth.Request(method="GET", url=request_url,is_form_encoded=True)
+
+    req['oauth_timestamp'] = oauth.Request.make_timestamp()
+    req['oauth_nonce'] = oauth.Request.make_nonce()
+    sig_method = oauth.SignatureMethod_HMAC_SHA1()
+
+    req.sign_request(sig_method, consumer, token=None)
+
+    # print req.to_url()
+
+    buffer = BytesIO()
+
+    curlGET = pycurl.Curl()
+    curlGET.setopt(curlGET.URL, req.to_url())
+    curlGET.setopt(curlGET.WRITEDATA, buffer)
+    curlGET.perform()
+    curlGET.close()
+
+    with open(filename, 'wb') as f:
+        f.write(buffer.getvalue())
---
+++ src/fftm.py
@@ -0,0 +1,14 @@
+import musicbrainzngs as m
+import download7Dpreview
+
+# Musicbrainzngs app setup
+m.set_useragent(
+    "fftm",
+    "0.1",
+    "https://github.com/jorgegarcia/mhdlondon2014/",
+)
+
+print m.search_artists( 'The Gap Band' )
+
+# Testing out downloading from 7D
+download7Dpreview.write7DPreview( "testdownload.mp3", 1234 )
