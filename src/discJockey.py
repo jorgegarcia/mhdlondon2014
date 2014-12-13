5bbebf53fcfd5f1a9d17e71c09f15a5824f41497
Aristotel Digenis <greekgoddj@hotmail.com>
2014-12-13 20:14:02 +0000

    The discJockey file now makes an introductory announcement, with a variety of phrases to keep it "fresh"

--- src/discJockey.py
+++ src/discJockey.py
@@ -2,6 +2,7 @@ import os;
 import audio
 import time
 import threading
+import random
 
 sampleDuration = 10
 fadeDuration = 1
@@ -16,10 +17,31 @@ def MakeAnnouncement(phrase):
 def SpinRecord(file, volume):
     audio.PlayFile(file, volume)
 
-for f in downloadedFiles:
-    announcementThread = threading.Thread(target=MakeAnnouncement, args=[f])
-    announcementThread.start()
+def AnnounceSetlist(category, yearStart, yearEnd):
+    intro = ("Welcome", "Greetings", "Wazzzzzzz-up")
+    audience = ("party people", "fans")
+    body = ("let's listen to some " + category + " tracks between " + str(yearStart) + " and " + str(yearEnd),
+            "here are the " + category + " tunes from " + str(yearStart) + " and the following " + str(yearEnd - yearStart) + " years")
 
-    songThread = threading.Thread(target=SpinRecord, args=[os.path.join(downloadedPath, f), 0.5])
-    songThread.start()
-    time.sleep(sampleDuration - (fadeDuration * 0.5))
+    finalMessage = intro[random.randint(0, len(intro)-1)]
+    finalMessage += " "
+    finalMessage += audience[random.randint(0, len(audience)-1)]
+    finalMessage += ","
+    finalMessage += body[random.randint(0, len(body)-1)]
+    finalMessage += "."
+    print finalMessage
+
+    audio.PlayTextToSpeech(finalMessage)
+
+def PlaySetlist():
+    for f in downloadedFiles:
+        announcementThread = threading.Thread(target=MakeAnnouncement, args=[f])
+        announcementThread.start()
+
+        songThread = threading.Thread(target=SpinRecord, args=[os.path.join(downloadedPath, f), 0.5])
+        songThread.start()
+        time.sleep(sampleDuration - (fadeDuration * 0.5))
+
+
+AnnounceSetlist("Motown", 1981, 2014)
+PlaySetlist()
