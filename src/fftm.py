7d18f22967df3dff336cbd07c85b011b5852e378
Jorge Garcia <info@jorgegarciamartin.com>
2014-12-13 16:15:54 +0000

    more testing for getting tracks from 7Digital

--- src/fftm.py
+++ src/fftm.py
@@ -1,14 +1,33 @@
-import musicbrainzngs as m
+import musicbrainzngs as mbrainz
 import download7Dpreview
+import py7D
+import json
 
 # Musicbrainzngs app setup
-m.set_useragent(
+mbrainz.set_useragent(
     "fftm",
     "0.1",
     "https://github.com/jorgegarcia/mhdlondon2014/",
 )
 
-print m.search_artists( 'The Gap Band' )
+artistName = "The Gap Band"
 
-# Testing out downloading from 7D
-download7Dpreview.write7DPreview( "../data/downloaded/testdownload.mp3", 1234 )
+def track_previews(artistName):
+
+    response = py7D.request('track', 'search', q=artistName, pageSize=3)
+    tracks = response['response']['searchResults']['searchResult']
+
+
+    for track in tracks:
+        print track
+        track['preview'] = py7D.preview_url(track['track']['@id'])
+
+    return tracks
+
+
+if __name__ == "__main__":
+    print mbrainz.search_artists(artistName)
+    print json.dumps(track_previews(artistName), indent=4)
+
+    # Testing out downloading from 7D
+    download7Dpreview.write7DPreview( "../data/downloaded/testdownload.mp3", 33499156 )
